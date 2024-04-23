9414446d99da3c398929159edef114dcf159233b
Paloma Souza <paloma.souza@ambevtech.com.br>
2024-04-22 22:42:18 -0300

    first commit

---
+++ 01_APICatalago/APICatalago/Domain/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace APICatalago.Domain;
+
+// Classes com apenas propriedades - sem compartamentos - classes anêmicas
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Configurando projeto para usar o EF Core
+Pomelo.EntityFrameworkCore.MySql
+Microsoft.EntityFrameworkCore.Design
+
+dotnet tool install --global dotnet-ef
+
+dotnet tool update --global dotnet-ef
