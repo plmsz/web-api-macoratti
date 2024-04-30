dcb532a92173168351f1552b07d08bb13f1070d0
Paloma Souza <paloma.souza@ambevtech.com.br>
2024-04-29 23:01:37 -0300

    add produtoscontroller

---
+++ 01_APICatalago/APICatalago/Controllers/ProdutosController.cs
@@ -0,0 +1,91 @@
+﻿using APICatalago.Context;
+using APICatalago.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICatalago.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // IEnumerable => interface de leitura, não precisa ter toda coleção na mémoria, permite adiar a execução ( trabalha sobre demanda
+
+        //actioresult pode retornar Ienumerable<produto> (um tipo qualquer) ou uma instância actionresult
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados.");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+            return produto;
+        }
+
+        //ApiController - permite inferencia do body e do request
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+            //Response headers com location  location: https://localhost:7279/Produtos/{id
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            //var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            var produto = _context.Produtos.Find(id); // chave primária
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
